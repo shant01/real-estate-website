6e4778ea2a040ca0c6798cf7803b1e7a96f49d2b
shant01 <shanthovagimian@gmail.com>
2023-11-25 17:34:34 -0800

    Creating layout of website

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from "./Container";
+
+function About() {
+  return (
+    <div className="bg-white text-gray-800 p-8">
+      <Container>
+        <div className="container mx-auto">
+          <div className="animate-slideIn">
+            <h2 className="text-3xl font-semibold mb-4">
+              About Jon Doe Realty
+            </h2>
+            <p className="text-lg">
+              Jon Doe Realty has been serving the community for over 20 years,
+              providing exceptional real estate services and helping clients
+              find their dream homes.
+            </p>
+            {/* More about content */}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default About;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,9 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
+
 }
 
 
